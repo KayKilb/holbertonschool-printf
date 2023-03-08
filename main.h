43e693abe68aa5928f55370b9c78016c4e538204
krisCrossApplesauce <karisrr2203@gmail.com>
2023-03-08 12:36:27 -0800

    printf.c: im just guessing really

--- main.h
+++ main.h
@@ -1,6 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * struct spec - struct spec
+ *
+ * @spec: the specifier
+ * @f: the function associated
+ */
+typedef struct spec
+{
+	char *spec;
+	int (*f)(int a, int b);
+} spec_t;
+
+int _printf(const char *format, ...);
 
 #endif
--- printf.c
+++ printf.c
@@ -1,13 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _printf -
+ * _printf - returns the number of characters printed
+ * excluding the null byte used to end output to strings
+ * ^write output to stdout, the standard output stream
  *
- * @format: 
+ * @format: char array, the string to be printed
  *
- * Return: 
+ * Return: int
  */
 
 int _printf(const char *format, ...)
 {
+	va_list = ap;
+	int i = 0, ii = 0, x = 0, sl = 0;
+	spec_t specs[] = {
+		{ "c", print_char },
+		{ "s", print_string }
+	};
+
+	if (format == NULL)
+	{ return (0); }
+
+	va_start(ap, format);
+
+	while (format[i])
+	{
+		ii = 0;
+
+		if (format[i] == "%" && x == 0)
+		{
+			x = 1;
+		}
+		else if (x == 1)
+		{
+			while (ii < 2)
+			{
+				if (format[i] == specs[ii].spec[0])
+				{
+					specs[ii].f(ap);
+					sl == sl + specs[ii].f(ap);
+					x == 2;
+				}
+				ii++;
+			}
+			
+			if (x != 2)
+			{ putchar(format[i]); }
+			x = 0;
+		}
+		else
+		{ putchar(format[i]); }
+
+		i++;
+		sl++;
+	}
+
+	va_end(ap);
+
+	return(sl);
+}
+
+
+/**
+ * print_char - prints a char
+ *
+ * @ap: a variadic argument
+ *
+ * Return: int
+ */
+int print_char(va_list ap)
+{
+	putchar(va_arg(ap, char));
+	return (0);
+}
+
+/**
+ * print_string - prints a string
+ *
+ * @ap: a variadic argument
+ *
+ * Return: int
+ */
+int print_string(va_list ap)
+{
+	int sl = 0;
+	char *str;
+
+	str = va_arg(ap, char *);
+
+	while (str[sl])
+	{
+		putchar(va_arg(ap[sl], char));
+		sl++;
+	}
+
+	sl = sl - 1;
+	return (sl);
 }
